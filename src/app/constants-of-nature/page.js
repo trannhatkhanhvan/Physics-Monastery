9024a3de75d636c02d3595258879267f9bd7ea4f
Thad Roberts <thadroberts@Thads-Mac-mini.local>
2026-05-25 12:50:02 -0600

    Update site

--- src/app/constants-of-nature/page.js
+++ src/app/constants-of-nature/page.js
@@ -30708,6 +30708,17 @@ J ⋮ kg{" "}
                     MHz = the megahertz,{" "}
 
                     <img
+                        src="/equations/second.svg"
+                        alt="s"
+                        style={{
+                            height: "0.55em",
+                            verticalAlign: "-0.0em",
+                            display: "inline",
+                        }}
+                    />{" "}
+                    = the second,{" "}
+
+                    <img
                         src="/equations/planck_charge.svg"
                         alt="q_p"
                         style={{
@@ -34956,6 +34967,17 @@ J ⋮ kg{" "}
                         }}
                     />{" "}
                     = the Planck length,{" "}
+
+                        <img
+                        src="/equations/second.svg"
+                        alt="s"
+                        style={{
+                            height: "0.55em",
+                            verticalAlign: "-0.0em",
+                            display: "inline",
+                        }}
+                    />{" "}
+                    = the second,{" "}
 
                     <img
                         src="/equations/planck_time.svg"
